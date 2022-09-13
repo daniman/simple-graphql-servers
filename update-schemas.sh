d9830e6f84b654e223a0ad779599ba6172c85e78
Danielle Man <man.danielleh@gmail.com>
2022-09-13 15:17:29 -0500

    update update-schema script

--- update-schemas.sh
+++ update-schemas.sh
@@ -3,3 +3,7 @@
 rover graph introspect https://simple-graphql-servers.netlify.app/.netlify/functions/subgraph-congress | rover graph publish simple-servers@congress --schema -
 rover graph introspect https://simple-graphql-servers.netlify.app/.netlify/functions/subgraph-address-enrichment | rover graph publish simple-servers@address-enrichment --schema -
 rover graph introspect https://simple-graphql-servers.netlify.app/.netlify/functions/subgraph-daylight-times | rover graph publish simple-servers@daylight-times --schema -
+
+rover subgraph introspect https://simple-graphql-servers.netlify.app/.netlify/functions/subgraph-congress | rover subgraph publish simple-servers@supergraph --name congress --schema - --routing-url https://simple-graphql-servers.netlify.app/.netlify/functions/subgraph-congress
+rover subgraph introspect https://simple-graphql-servers.netlify.app/.netlify/functions/subgraph-address-enrichment | rover subgraph publish simple-servers@supergraph --name address-enrichment --schema - --routing-url https://simple-graphql-servers.netlify.app/.netlify/functions/subgraph-address-enrichment
+rover subgraph introspect https://simple-graphql-servers.netlify.app/.netlify/functions/subgraph-daylight-times | rover subgraph publish simple-servers@supergraph --name daylight-times --schema - --routing-url https://simple-graphql-servers.netlify.app/.netlify/functions/subgraph-daylight-times  
